b48f52c6be08ac0d49f934f1ae03ceef29b147a1
Rounak Jain <rounakjainofficial@gmail.com>
2022-08-23 19:33:57 +0530

    moved utils out of components

--- src/components/BreakCounter/index.jsx
+++ src/components/BreakCounter/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { countBreaksWithAirports } from "../utils/breakCounter";
+import { countBreaksWithAirports } from "../../utils/breakCounter";
 
 const Grid = styled.div`
   display: grid;
